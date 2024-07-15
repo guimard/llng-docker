4d5c1cc6c91c2416fa3ec6a319b74fa93821beb2
Yadd <yadd@debian.org>
2024-07-15 16:13:41 +0400

    Adapt backup filenames

--- sessions-backup/start.sh
+++ sessions-backup/start.sh
@@ -4,14 +4,17 @@ set -e
 
 mkdir -p /var/backup/lemonldap-ng
 
-FILE="/var/backup/lemonldap-ng/$(date --utc +'%F_%H:%M:%S')-sessions-backup.json"
+FILE="/var/backup/lemonldap-ng/$(date --utc +'%F_%H:%M:%S')-sessions-backup"
 OPTS='--user www-data --group www-data'
 
 if test "$BACKUP" == "persistent"; then
 	OPTS="$OPTS --persistent"
+	FILE="$FILE-persistent"
 elif test "$BACKUP" == "refresh_tokens"; then
 	OPTS="$OPTS --refresh-tokens"
+	FILE="$FILE-oidc-refresh-tokens"
 fi
+FILE="$FILE.json"
 
 /usr/share/lemonldap-ng/bin/lemonldap-ng-sessions \
 	$OPTS \
