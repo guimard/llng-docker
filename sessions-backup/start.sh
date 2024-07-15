e7c2ea8aa893c1060fd9a4448e52eb4e2c2ab6f7
Yadd <yadd@debian.org>
2024-07-15 15:45:48 +0400

    Add restrcted backup

--- sessions-backup/start.sh
+++ sessions-backup/start.sh
@@ -5,10 +5,16 @@ set -e
 mkdir -p /var/backup/lemonldap-ng
 
 FILE="/var/backup/lemonldap-ng/$(date --utc +'%F_%H:%M:%S')-sessions-backup.json"
+OPTS='--user www-data --group www-data'
+
+if test "$BACKUP" == "persistent"; then
+	OPTS="$OPTS --persistent"
+elif test "$BACKUP" == "refresh_tokens"; then
+	OPTS="$OPTS --refresh-tokens"
+fi
 
 /usr/share/lemonldap-ng/bin/lemonldap-ng-sessions \
-	--user www-data \
-	--group www-data \
+	$OPTS \
 	backup \
 	> $FILE
 
