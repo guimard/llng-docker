0efd65f44ef345f697dbf11c5c1c8dce4b8c75bb
Yadd <yadd@debian.org>
2024-08-16 16:41:37 +0400

    Update test

--- portal/__test__/portal.test.js
+++ portal/__test__/portal.test.js
@@ -3,6 +3,13 @@
 const supertest = require('supertest');
 const assert = require('assert');
 const request = supertest.agent('http://auth.example.com');
+const express = require('express');
+
+const app = express();
+app.get('/', function (req, res) {
+  res.status(200).json({ name: 'john' });
+})
+app.listen(3000)
 
 describe('LLNG portal', () => {
   before(function (done) {
@@ -56,9 +63,14 @@ describe('LLNG portal', () => {
 describe('RELAY', () => {
   const request = supertest.agent('http://foo.example.com')
   it('should find /languages/fr.json', (done) => {
-    request.get('/languages/fr.json')
+    request.get('/')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
       .expect(200)
-      .then(res => { done() })
+      .then(res => {
+        expect(res.body.name).toEqual('john');
+        done();
+      })
       .catch(done)
   });
 })
